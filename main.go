bd7ecbb7227fa7804e43a6376f2b990c2153868a
Trhaokiet.1303 <trhaokiet.1303@gmail.com>
2024-03-13 13:38:14 +0700

    1

--- main.go
+++ main.go
@@ -17,11 +17,6 @@ import (
 	"github.com/corpix/uarand"
 	"github.com/gookit/color"
 )
-import os
-import colorama
-from colorama import Fore
-
-colorama.init()
 
 var (
 	referers []string = []string{
@@ -100,12 +95,11 @@ func main() {
 	flag.StringVar(&hostname, "hostname", "", "example: --hostname https://example.com")
 	flag.Parse()
 
-	if len(hostname) == 0:
-		colorama.Fore.RED.print("Missing hostname.")
+	if len(hostname) == 0 {
+		color.Red.Println("Missing hostname.")
+		color.Blue.Println("Example usage:\n\t ./getblaze --hostname https//:haokiet.click") //sửa hostname thành tên miền cần DDoS
 		os.Exit(1)
-	else:
-		domain = input("Nhập tên miền: ")
-		colorama.Fore.BLUE.print(f"Example usage:\n\t ./getblaze --hostname https//:{domain}")
+	}
 
 	color.Yellow.Println("Press control+c to stop")
 	time.Sleep(2 * time.Second)
